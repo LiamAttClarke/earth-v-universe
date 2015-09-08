2533ae7bc99454800c83dff6b4fe5f69ce604823
LiamAttClarke <liam.att.clarke@gmail.com>
2015-09-08 02:05:13 -0400

    Added physijs library, updated scene for physijs compatibility.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,8 +1,12 @@
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight ); 
-document.body.appendChild( renderer.domElement );
+// Debugging
+var debug = document.getElementById("debug");
+
+Physijs.scripts.worker = 'physijs_worker.js';
+//Physijs.scripts.ammo = '/js/ammo.js';
+
+var scene, camera, renderer;
+var frameRate = 30;
+var fieldOfView = 60;
 
 var deg2Rad = Math.PI / 180;
 var rad2Deg = 180 / Math.PI;
@@ -13,95 +17,117 @@ var camSettings = {
 	minLatitude: 5,
 	maxLatitude: 175
 };
-var deviceData = {
-	
-};
-var cube, cube2;
 
+// Scene Objects
+var planet;
+
+window.onload = initialize;
+// called before start
+function initialize() {
+	scene = new Physijs.Scene();
+	camera = new THREE.PerspectiveCamera(fieldOfView, window.innerWidth / window.innerHeight, 0.1, 1000);
+	renderer = new THREE.WebGLRenderer();
+	renderer.setSize( window.innerWidth, window.innerHeight ); 
+	document.body.appendChild( renderer.domElement );
+	start();
+}
+
+// start of game
 function start() {
-	// initialization
-	var geometry = new THREE.BoxGeometry(1, 1, 1);
-	var material = new THREE.MeshNormalMaterial();
-	cube = new THREE.Mesh(geometry, material);
-	cube2 = new THREE.Mesh(geometry, material);
-	scene.add(cube);
-	scene.add(cube2);
-	cube2.position.y += 1;
-	cube2.scale.set(1, 0.25, 0.25);
+	/*planet = new Physijs.SphereMesh(
+		new THREE.SphereGeometry(0.5, 24, 24),
+		new THREE.MeshNormalMaterial(),
+		0
+	);*/
+	planet = new Physijs.BoxMesh(
+		new THREE.BoxGeometry(1, 1, 1),
+		new THREE.MeshNormalMaterial(),
+		0
+	);
+	scene.add( planet );
 	
-	render();
+	// begin render loop
+	update();
 }
-start();
 
-var debug = document.getElementById("debug");
-function render() { 
-	requestAnimationFrame( render ); 
-	
-	// update loop
+// Main Render Loop
+function update() {
+	// player orientation
 	camera.position.set( camSettings.position.x, camSettings.position.y, camSettings.position.z);
-	camera.lookAt(cube.position);
-	//camera.rotation.z = 0; //camSettings.rotationZ;
+	camera.lookAt(planet.position);
 	
+	// render / physics frame calls
+	scene.simulate();
 	renderer.render( scene, camera ); 
+	// limit framerate
+	setTimeout( function() {
+        requestAnimationFrame( update );
+    }, 1000 / frameRate ); 
 }
 
+
+// Device Orientation
 var promise = FULLTILT.getDeviceOrientation({'type': 'world'});
 window.addEventListener("deviceorientation", function(event) {
 	promise.then(function(deviceOrientation) {
-		
 		// Use `deviceOrientation' object to interact with device orientation sensors
-		var orientation = deviceOrientation.getScreenAdjustedEuler();
-		// Latitude
-		var lat = orientation.beta;
-		if(orientation.beta < 0) {
-			if(orientation.beta > -90) {
-				lat = camSettings.minLatitude;
-			} else {
-				lat = camSettings.maxLatitude;
-			}
-		}
-		
-		// Longitude
-		var alphaRad = orientation.alpha * deg2Rad;
-	 	var betaRad = orientation.beta * deg2Rad;
-		var gammaRad = orientation.gamma * deg2Rad;
-	  	
-		//Calculate equation component
-		var cA = Math.cos(alphaRad);
-		var sA = Math.sin(alphaRad);
-		var sB = Math.sin(betaRad);
-		var cG = Math.cos(gammaRad);
-		var sG = Math.sin(gammaRad);
-		
-		//Calculate A, B, C rotation components
-		var rA = - cA * sG - sA * sB * cG;
-		var rB = - sA * sG + cA * sB * cG;
-		
-		//Calculate compass heading
-		var compassHeading = Math.atan(rA / rB);
-		
-		//Convert from half unit circle to whole unit circle
-		if(rB < 0) {
-			compassHeading += Math.PI;
-		}else if(rA < 0) {
-			compassHeading += 2 * Math.PI;
-		}
-		
-		//Convert radians to degrees
-		compassHeading *= rad2Deg;
-		
-		//update latitude
-		var lon = compassHeading;
-		
-		camSettings.position.x = camSettings.orbitRadius * Math.sin(lat * deg2Rad) * Math.cos(lon * deg2Rad);
-		camSettings.position.y = camSettings.orbitRadius * Math.cos(lat * deg2Rad);
-		camSettings.position.z = camSettings.orbitRadius * Math.sin(lat * deg2Rad) * Math.sin(lon * deg2Rad);
+		var deviceRotation = deviceOrientation.getScreenAdjustedEuler();
+		var newCamPos = calculatePlayerVector(deviceRotation, camSettings.orbitRadius);
+		camSettings.position.set(newCamPos.x, newCamPos.y, newCamPos.z);
 		
 	}).catch(function(message) {
 		// Device Orientation Events are not supported
 		console.log("This Device is not supported.");
 	});
 });
+
+// Player vector based on device orientation
+function calculatePlayerVector(deviceRotation, radius) {
+	// LATITUDE
+	var latitude = deviceRotation.beta * deg2Rad; // radians
+	if(deviceRotation.beta < 0) {
+		if(deviceRotation.beta > -90) {
+			latitude = camSettings.minLatitude;
+		} else {
+			latitude = camSettings.maxLatitude;
+		}
+	}
+	
+	// LONGITUDE
+	var alphaRad = deviceRotation.alpha * deg2Rad;
+	var betaRad = deviceRotation.beta * deg2Rad;
+	var gammaRad = deviceRotation.gamma * deg2Rad;
+	
+	// Calculate equation component
+	var cosAlpha = Math.cos(alphaRad);
+	var sinAlpha = Math.sin(alphaRad);
+	var sinBeta = Math.sin(betaRad);
+	var cosGamma = Math.cos(gammaRad);
+	var sinGamma = Math.sin(gammaRad);
+	
+	// Calculate A, B, C rotation components
+	var rotationAlpha = - cosAlpha * sinGamma - sinAlpha * sinBeta * cosGamma;
+	var rotationBeta = - sinAlpha * sinGamma + cosAlpha * sinBeta * cosGamma;
+	
+	//Calculate compass heading
+	var compassHeading = Math.atan(rotationAlpha / rotationBeta);
+	
+	//Convert from half unit circle to whole unit circle
+	if(rotationBeta < 0) {
+		compassHeading += Math.PI;
+	}else if(rotationAlpha < 0) {
+		compassHeading += 2 * Math.PI;
+	}
+	
+	var longitude = compassHeading; // Radians
+	
+	// final player vector
+	var x = radius * Math.sin(latitude) * Math.cos(longitude);
+	var y = radius * Math.cos(latitude);
+	var z = radius * Math.sin(latitude) * Math.sin(longitude);
+	
+	return new THREE.Vector3(x, y, z);
+}
 
 /*function clamp(value, min, max) {
 	if(value < min) {
