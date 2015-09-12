25053cb0ddfe6d110de6df2938aea3f71c743560
LiamAttClarke <liam.att.clarke@gmail.com>
2015-09-12 14:19:29 -0400

    redesigned gui

--- server.js
+++ server.js
@@ -19,7 +19,42 @@ server.listen(1337, function() {
 	console.log("Listening on port %s", port);
 });
 
+function Room(id) {
+	this.id = id;
+	this.players = [id];
+}
+var rooms = [];
+
 // new connection event
-io.on("connection", function(socket) {
-	console.log(socket.id);
+io.sockets.on("connection", function(socket) {
+	console.log(socket.id + " - connected");
+	var room = joinOrCreateRoom(socket.id);
+	socket.join(room.id);
+	
+	// connection lost event
+	socket.on( "disconnect", function() {
+		console.log(socket.id + " - disconnected");
+		leaveRoom(socket, room);
+	});
 });
+
+// searches for open rooms to join or creates new room
+function joinOrCreateRoom(clientId) {
+	var firstAvailableRoom = rooms.filter(function(room) { return room.players.length === 1; })[0];
+	if(!firstAvailableRoom) {
+		var newRoom = new Room(clientId);
+		rooms.push( newRoom );
+		return newRoom;
+	}
+	firstAvailableRoom.players.push( clientId );
+	return firstAvailableRoom;
+}
+
+// remove socket from room then remove room if empty
+function leaveRoom(socket, room) {
+	socket.leave(room.id);
+	room.players.splice(room.players.indexOf(socket.id), 1);
+	if(room.players.length === 0) {
+		rooms.splice(rooms.indexOf(room), 1);
+	}
+}
