821e93ab5c9bc90b8fb70ebc719e5dd93a72b784
LiamAttClarke <liam.att.clarke@gmail.com>
2015-09-11 23:58:15 -0400

    Added socket.io

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,131 +1,144 @@
 'strict';
 
-// 
-var scene, camera, renderer;
-var frameRate = 30;
-var camSettings = {
-	fieldOfView: 60,
-	orbitRadius: 3,
-	cameraQuaternion: new THREE.Quaternion()
-};
-var deviceData = {};
-var player = {};
-var opponent = {};
-
-// Scene Objects
-var planet;
-
-var setObjectRotation = function() {
-	var axisZ = new THREE.Vector3( 0, 0, 1 );
-	var euler = new THREE.Euler();
-	var q0 = new THREE.Quaternion();
-	var root2Over2 = Math.sqrt( 0.5 );
-	var q1 = new THREE.Quaternion( -root2Over2, 0, 0, root2Over2 ); // - PI/2 around the x-axis
-	return function(quaternion, alpha, beta, gamma, orientation) {
-		euler.set( beta, alpha, -gamma, 'YXZ' );                       		  // 'ZXY' for the device, but 'YXZ' for us
-		quaternion.setFromEuler( euler );                               	  // orient the device
-		quaternion.multiply( q1 );                                      	  // camera looks out the back of the device, not the top
-		quaternion.multiply( q0.setFromAxisAngle( axisZ, -orientation ) );    // adjust for screen orientation
+(function() {
+	// Dependencies
+	var THREE = require("three");
+	var Physijs = require("physijs");
+	var THREE = require("three");
+	var THREE = require("three");
+	var THREE = require("three");
+	
+	var scene, camera, renderer;
+	var frameRate = 30;
+	var camSettings = {
+		fieldOfView: 60,
+		orbitRadius: 3,
+		cameraQuaternion: new THREE.Quaternion()
 	};
-}();
-
-// called before start
-function initialize() {
-	Physijs.scripts.worker = 'physijs_worker.js';
-	Physijs.scripts.ammo = 'ammo.js';
-	scene = new Physijs.Scene();
-	camera = new THREE.PerspectiveCamera(camSettings.fieldOfView, window.innerWidth / window.innerHeight, 0.1, 1000);
-	renderer = new THREE.WebGLRenderer();
-	renderer.setSize( window.innerWidth, window.innerHeight ); 
-	document.body.appendChild( renderer.domElement );
+	var deviceData = {};
 	
-	window.addEventListener("resize", function() {
-		camera.aspect = window.innerWidth / window.innerHeight;
-		camera.updateProjectionMatrix();
-		renderer.setSize( window.innerWidth, window.innerHeight );
-	}, false);
+	// Scene Objects
+	var planet;
 	
-	// Device orientation
-	window.addEventListener("deviceorientation", function(event) {
-		deviceData = event;
-	}, false);
+	var setObjectRotation = function() {
+		var axisZ = new THREE.Vector3( 0, 0, 1 );
+		var euler = new THREE.Euler();
+		var q0 = new THREE.Quaternion();
+		var root2Over2 = Math.sqrt( 0.5 );
+		var q1 = new THREE.Quaternion( -root2Over2, 0, 0, root2Over2 ); // - PI/2 around the x-axis
+		return function(quaternion, alpha, beta, gamma, orientation) {
+			euler.set( beta, alpha, -gamma, 'YXZ' );                       		  // 'ZXY' for the device, but 'YXZ' for us
+			quaternion.setFromEuler( euler );                               	  // orient the device
+			quaternion.multiply( q1 );                                      	  // camera looks out the back of the device, not the top
+			quaternion.multiply( q0.setFromAxisAngle( axisZ, -orientation ) );    // adjust for screen orientation
+		};
+	}();
 	
-	start();
-} window.onload = initialize;
-
-// start of game
-function start() {
-	// Skybox
-	(function() {
-		var urlPrefix = "assets/textures/skybox/test/";
-		var cubeMapFaces = [
-			urlPrefix + "2.png",
-			urlPrefix + "3.png",
-			urlPrefix + "1.png",
-			urlPrefix + "6.png",
-			urlPrefix + "4.png",
-			urlPrefix + "5.png"
-		];
-		var textureCube = THREE.ImageUtils.loadTextureCube(cubeMapFaces);
-		var skyShader = THREE.ShaderLib[ "cube" ];
-		skyShader.uniforms[ "tCube" ].value = textureCube;
-		var cubeMapSize = 256;
-		var skyBox = new THREE.Mesh(
-			new THREE.BoxGeometry(cubeMapSize, cubeMapSize, cubeMapSize),
-			new THREE.ShaderMaterial({
-				vertexShader: skyShader.vertexShader,
-				fragmentShader: skyShader.fragmentShader,
-				uniforms: skyShader.uniforms,
-				depthWrite: false,
-				side: THREE.BackSide
-			})
-		);
-		scene.add( skyBox );
-	})();
+	// called before start
+	function initialize() {
+		Physijs.scripts.worker = 'physijs_worker.js';
+		Physijs.scripts.ammo = 'ammo.js';
+		scene = new Physijs.Scene();
+		camera = new THREE.PerspectiveCamera(camSettings.fieldOfView, window.innerWidth / window.innerHeight, 0.1, 1000);
+		renderer = new THREE.WebGLRenderer();
+		renderer.setSize( window.innerWidth, window.innerHeight ); 
+		document.body.appendChild( renderer.domElement );
+		
+		window.addEventListener("resize", function() {
+			camera.aspect = window.innerWidth / window.innerHeight;
+			camera.updateProjectionMatrix();
+			renderer.setSize( window.innerWidth, window.innerHeight );
+		}, false);
+		
+		// Device orientation
+		window.addEventListener("deviceorientation", function(event) {
+			deviceData = event;
+		}, false);
+		
+		// P2P connection
+		var socket = io();
+		var p2p = new P2P(socket, { numClients: 2 });
+		p2p.on('peer-msg', function (data) {
+			console.log(data);
+		});
+		
+		start();
+	} window.onload = initialize;
 	
-	// Planet
-	camera.position.z += camSettings.orbitRadius;
-	/*planet = new Physijs.SphereMesh(
-		new THREE.SphereGeometry(0.5, 24, 24),
-		new THREE.MeshNormalMaterial(),
-		0
-	);*/
-	planet = new Physijs.BoxMesh(
-		new THREE.BoxGeometry(1, 1, 1),
-		new THREE.MeshNormalMaterial(),
-		0
-	); 
-	scene.add( planet );
+	// start of game
+	function start() {
+		// Skybox
+		(function() {
+			var urlPrefix = "assets/textures/skybox/test/";
+			var cubeMapFaces = [
+				urlPrefix + "2.png",
+				urlPrefix + "3.png",
+				urlPrefix + "1.png",
+				urlPrefix + "6.png",
+				urlPrefix + "4.png",
+				urlPrefix + "5.png"
+			];
+			var textureCube = THREE.ImageUtils.loadTextureCube(cubeMapFaces);
+			var skyShader = THREE.ShaderLib[ "cube" ];
+			skyShader.uniforms[ "tCube" ].value = textureCube;
+			var cubeMapSize = 256;
+			var skyBox = new THREE.Mesh(
+				new THREE.BoxGeometry(cubeMapSize, cubeMapSize, cubeMapSize),
+				new THREE.ShaderMaterial({
+					vertexShader: skyShader.vertexShader,
+					fragmentShader: skyShader.fragmentShader,
+					uniforms: skyShader.uniforms,
+					depthWrite: false,
+					side: THREE.BackSide
+				})
+			);
+			scene.add( skyBox );
+		})();
+		
+		// Planet
+		camera.position.z += camSettings.orbitRadius;
+		/*planet = new Physijs.SphereMesh(
+			new THREE.SphereGeometry(0.5, 24, 24),
+			new THREE.MeshNormalMaterial(),
+			0
+		);*/
+		planet = new Physijs.BoxMesh(
+			new THREE.BoxGeometry(1, 1, 1),
+			new THREE.MeshNormalMaterial(),
+			0
+		); 
+		scene.add( planet );
+		
+		// begin render loop
+		update();
+	}
 	
-	// begin render loop
-	update();
-}
-
-// Main Render Loop
-function update() {
-	// player orientation
-	var alpha = deviceData.alpha ? THREE.Math.degToRad( deviceData.alpha ) : 0; // Z
-	var beta = deviceData.beta ? THREE.Math.degToRad( deviceData.beta ) : 0; // X
-	var gamma = deviceData.gamma ? THREE.Math.degToRad( deviceData.gamma ) : 0; // Y
-	var orient = window.orientation ? THREE.Math.degToRad( window.orientation ) : 0; // O
-	var quaternion = new THREE.Quaternion();
-	setObjectRotation( quaternion, alpha, beta, gamma, orient );
-	var pos = (new THREE.Vector3( 0, 0, camSettings.orbitRadius ) ).applyQuaternion( quaternion );
-	camera.position.set(pos.x, pos.y, pos.z);
-	setObjectRotation( camera.quaternion, alpha, beta, gamma, orient );
+	// Main Render Loop
+	function update() {
+		// player orientation
+		var alpha = deviceData.alpha ? THREE.Math.degToRad( deviceData.alpha ) : 0; // Z
+		var beta = deviceData.beta ? THREE.Math.degToRad( deviceData.beta ) : 0; // X
+		var gamma = deviceData.gamma ? THREE.Math.degToRad( deviceData.gamma ) : 0; // Y
+		var orient = window.orientation ? THREE.Math.degToRad( window.orientation ) : 0; // O
+		var quaternion = new THREE.Quaternion();
+		setObjectRotation( quaternion, alpha, beta, gamma, orient );
+		var pos = (new THREE.Vector3( 0, 0, camSettings.orbitRadius ) ).applyQuaternion( quaternion );
+		camera.position.set(pos.x, pos.y, pos.z);
+		setObjectRotation( camera.quaternion, alpha, beta, gamma, orient );
+		
+		// render / physics frame calls
+		scene.simulate();
+		renderer.render( scene, camera ); 
+		// limit framerate
+		setTimeout( function() {
+			requestAnimationFrame( update );
+		}, 1000 / frameRate ); 
+	}
 	
-	// render / physics frame calls
-	scene.simulate();
-	renderer.render( scene, camera ); 
-	// limit framerate
-	setTimeout( function() {
-        requestAnimationFrame( update );
-    }, 1000 / frameRate ); 
-}
-
-// Debugging
-var debugElement = document.getElementById("debug");
-function debug(stringValue) {
-	console.log(stringValue);
-	debugElement.innerHTML = stringValue;
-}
+	// Debugging
+	var debugElement = document.getElementById("debug");
+	function debug(stringValue) {
+		console.log(stringValue);
+		debugElement.innerHTML = stringValue;
+	}
+})();
--- server.js
+++ server.js
@@ -1,15 +1,19 @@
-var http = require("http");
 var express = require("express");
-
 var app = express();
+var server = require("http").createServer(app);
+var io = require("socket.io")(server);
+var p2p = require('socket.io-p2p-server').Server;
+io.use(p2p);
 
+//set static folder for application
 app.use(express.static("public"));
-app.get("/", function(request, response) {
-	response.sendFile(__dirname + "public/index.html");
-});
-var server = app.listen(1337);
-var io = require("socket.io").listen(server);
 
-/*io.sockets.on("connection", function(client) {
-	console.log("connection", client);
-});*/
+// serve application
+app.get("/", function(request, response) {
+	response.sendFile(__dirname + "/index.html");
+});
+
+// open port 1337 for communication
+server.listen(1337, function() {
+	console.log("Listening on port 1337");
+});
