b5b2ff75b715e438376206aa5cafc3ac52c242ee
LiamAttClarke <liam.att.clarke@gmail.com>
2015-10-02 17:08:44 -0400

    stuff

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -8,8 +8,8 @@ window.onload = function() {
 	// Networking
 	//var dgram = require('dgram');
 	var io = require('socket.io-client');
-	//var socket = io.connect('https://romjam-liamattclarke.rhcloud.com:8443', {'forceNew':true});
-	var socket = io(); // local testing
+	var socket = io.connect('https://romjam-liamattclarke.rhcloud.com:8443', {'forceNew':true});
+	//var socket = io(); // local testing
 	
 	// Settings
 	var settings = {
