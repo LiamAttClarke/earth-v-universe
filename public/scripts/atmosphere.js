f9b5db48d0efe05caa52a63bbe9cafb5769a44a4
David Tran <cdavid.tran@gmail.com>
2015-10-04 07:06:34 -0400

    sdf

--- public/scripts/atmosphere.js
+++ public/scripts/atmosphere.js
@@ -33,11 +33,11 @@ exports.fragmentShaderSolid =
     'varying vec3 e;' +
     'varying vec3 n;' +
     'void main() {' +
-    'vec3 r = reflect( e, n );' +
+    'vec3 r = normalize(reflect( e, n ));' +
     'float m = 2. * sqrt( pow( r.x, 2. ) + pow( r.y, 2. ) + pow( r.z + 1., 2. ) );' +
     'vec2 vN = r.xy / m + .5;' +
     'vec3 base = texture2D( texture1, vN ).rgb;' +
     'vec3 color = texture2D( texture2, vUv ).rgb;' +
-    'gl_FragColor = vec4( color, 1.0 );' +
+    'gl_FragColor = vec4( base * color, 1.0 );' +
     '}';
 
